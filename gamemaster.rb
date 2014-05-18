b28ff6963d7a51eaf9a40dfd6cdc97820f2228c7
Vinkesh Banka <vinkesh.banka@gmail.com>
2014-05-18 23:58:08 +0530

    Bankas | Showing the numbers on screen

--- gamemaster.rb
+++ gamemaster.rb
@@ -1,12 +1,29 @@
-displayed_number = []
-rgen = Random.new
+class Master
+  def initialize()
+    @displayed_number = []
+    @interval = 1
+    @rgen = Random.new
+  end
 
-while(displayed_number.size != 99) do
-  new_number = rgen.rand(1..99)
-  unless displayed_number.include? new_number
-    displayed_number.push(new_number)
-    system("say #{new_number}")
-    puts new_number
-    sleep(2)
+  def printx(number)
+    if(@displayed_number.size % 10 == 0)
+      puts ""
+    end
+    print " #{number.to_s.rjust(2)}"
+  end
+
+  def start()
+    while(@displayed_number.size != 99) do
+      new_number = @rgen.rand(1..99)
+      unless @displayed_number.include? new_number
+        @displayed_number.push(new_number)
+        system("say #{new_number}")
+        printx(new_number)
+        sleep(@interval)
+      end
+    end
   end
 end
+
+Master.new.start
+
